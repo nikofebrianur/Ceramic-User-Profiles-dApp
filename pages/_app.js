0650f3727c8a562ad159367a3e2bd70f5f57d92a
Niko Febrianur <nikofebrianur@gmail.com>
2023-01-18 11:19:55 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import { Provider } from "@self.id/react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+function MyApp({ Component, pageProps }) {
+	return (
+	  <Provider client={{ ceramic: "testnet-clay" }}>
+		<Component {...pageProps} />;
+	  </Provider>
+	);
+  }
